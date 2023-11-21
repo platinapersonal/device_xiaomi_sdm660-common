3f762be62c3cea7c5ad3dc73b970fdd27f2ca7a8
Sabar <31942277+sabarop@users.noreply.github.com>
2023-11-21 09:16:43 +0700

    sdm660-common: rootdir: Use metadata_csum for /metadata
    This enhances ext4 stability.
    
    Co-Authored-By: Jaegeuk Kim <20465538+jaegeuk@users.noreply.github.com>

--- sdm660.mk
+++ sdm660.mk
@@ -430,8 +430,10 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/partitions/flash_super_dummy.sh:install/bin/flash_super_dummy.sh
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/partitions/flash_super_dummy.sh:install/bin/flash_super_dummy.sh
+PRODUCT_PACKAGES += \
+    e2fsck_ramdisk \
+    tune2fs_ramdisk \
+    resize2fs_ramdisk
 
 # Permissions
 PRODUCT_COPY_FILES += \
