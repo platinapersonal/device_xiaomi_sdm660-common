729a54d5bb35234be3916c3067bcb56558140313
Sabar <31942277+sabarop@users.noreply.github.com>
2024-01-08 21:02:19 +0700

    sdm660-common: Migrate to XiaomiDoze

--- sdm660.mk
+++ sdm660.mk
@@ -190,6 +190,10 @@ PRODUCT_PACKAGES += \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor
 
+# DeviceDoze
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # Display Device Config
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/displayconfig/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
