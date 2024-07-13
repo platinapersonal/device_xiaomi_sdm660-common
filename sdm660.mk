67b94767a16b91e831c7fcfa7df27f113df093e4
Sabar <31942277+sabarop@users.noreply.github.com>
2024-07-13 14:01:26 +0700

    sdm660-common: Fix Use QTI USB init configs from USB HAL repo

--- sdm660.mk
+++ sdm660.mk
@@ -282,7 +282,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.goodix.sh \
-    init.msm.usb.configfs.rc \
     init.qcom.post_boot.sh \
     init.qcom.early_boot.sh \
     init.qcom.sensors.sh \
