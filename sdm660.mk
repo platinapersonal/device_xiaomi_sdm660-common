2196a930c481707964bbf946805a679bda39a294
pix106 <sbordenave@gmail.com>
2024-09-14 11:50:00 +0200

    sdm660-common: Build mtdservice 1.2

--- sdm660.mk
+++ sdm660.mk
@@ -361,7 +361,7 @@ PRODUCT_COPY_FILES += \
 # Mlipay
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.mlipay@1.1.vendor \
-    vendor.xiaomi.hardware.mtdservice@1.0.vendor
+    vendor.xiaomi.hardware.mtdservice@1.2.vendor
 
 # Net
 PRODUCT_PACKAGES += \
