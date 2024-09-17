LineageOS-jason/android_device_xiaomi_sdm660-common

sdm660-common: Move to QTI health AIDL service
The health AIDL HAL service provides functionalities of charger,
therefore system charger at /system/bin/charger is deprecated.
On top of that, QTI health AIDL HAL service enables suspend by
default, the equivalent of setting ro.charger.enable_suspend
for legacy charger.

Change-Id: I59c23e7974cea1174b0161f31a535fa3afa1e5c9
Signed-off-by: Nanhumly <admin@warbler.cc>

sdm660.mk
@@ -217,13 +217,8 @@ PRODUCT_COPY_FILES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl:64 \
-    android.hardware.health@2.1-service
-
-ifneq ($(AB_OTA_UPDATER),true)
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl.recovery
-endif
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery
 
 # HIDL
 PRODUCT_PACKAGES += \

platinapersonal/device_xiaomi_sdm660-common

Signed-off-by: pix106 <sbordenave@gmail.com>

sdm660.mk
@@ -37,7 +37,7 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-impl.recovery \
     android.hardware.boot@1.0-service \
-    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health-service.qti_recovery \
     bootctrl.sdm660 \
     bootctrl.sdm660.recovery
 
@@ -254,8 +254,7 @@ PRODUCT_PACKAGES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl-qti \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.qti
 
 # HIDL
 PRODUCT_PACKAGES += \