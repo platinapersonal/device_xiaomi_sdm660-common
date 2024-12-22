sotodrom/android_device_asus_sdm660-common

sdm660-common: prepare to move HALs to in-tree
Co-authored-by: Erfan Abdi <erfangplus@gmail.com>
Co-authored-by: @zeelog
Change-Id: I4920010472148bc1618bfc27029e145988462b72

sdm660.mk
@@ -13,6 +13,9 @@ PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
 # Default is nosdcard, S/W button enabled in resource
 PRODUCT_CHARACTERISTICS := nosdcard
 
+# Common Tree Path
+COMMON_PATH := device/asus/sdm660-common
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
@@ -434,6 +437,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client
+
+QCOM_SOONG_NAMESPACE := \
+    $(COMMON_PATH)/qcom-caf
 
 # Telephony
 PRODUCT_PACKAGES += \

platinapersonal/device_xiaomi_sdm660-common

sdm660.mk
@@ -58,6 +58,9 @@ PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 endif
 
+# Common Tree Path
+COMMON_PATH := device/xiaomi/sdm660-common
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
@@ -566,6 +569,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/xiaomi \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client
+
+QCOM_SOONG_NAMESPACE := \
+    $(COMMON_PATH)/qcom-caf
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile